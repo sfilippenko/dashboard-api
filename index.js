ace2094429c1704a38cd70f79cdb0634f4312b56
sfilippenko <fil_mf@mail.ru>
2025-02-06 15:35:32 +0300

    express

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = 8000;
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log('server started');
+})
